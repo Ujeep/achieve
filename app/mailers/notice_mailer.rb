b6a938db381224cab64fa42df9674eaae570dbe1
宇治祐介 <ujiyusuke@ujiyuusuke-no-MacBook-Pro.local>
2017-05-01 13:30:55 +0900

    mail

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -15,7 +15,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_contact(contact)
     @contact=contact
 
-  mail to: "yusukeuji@gmail.com",
+  mail to: @contact.email,
   subject:'【Achieve】問い合わせが投稿されました'
 end
 end
