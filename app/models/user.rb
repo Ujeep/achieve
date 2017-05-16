1a3cc31583823ce312dd4383bf912386551042c9
宇治祐介 <ujiyusuke@ujiyuusuke-no-MacBook-Pro.local>
2017-05-16 20:08:22 +0900

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:confirmable,:omniauthable
 
-mount_uploader :avatar, AvatarUploader
+  mount_uploader :avatar, AvatarUploader
          has_many :blogs
 
-         def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    user = User.find_by(email: auth.info.email)
+   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+             user = User.find_by(provider: auth.provider, uid: auth.uid)
 
-    unless user
+   unless user
       user = User.new(
           name:     auth.extra.raw_info.name,
           provider: auth.provider,
@@ -25,10 +25,10 @@ mount_uploader :avatar, AvatarUploader
     user
   end
 
-  def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
+   def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
       user = User.find_by(provider: auth.provider, uid: auth.uid)
 
-      unless user
+   unless user
         user = User.new(
             name:     auth.info.nickname,
             image_url: auth.info.image,
